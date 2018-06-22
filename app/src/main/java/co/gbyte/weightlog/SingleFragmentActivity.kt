fc355a632dc800fa372effb271a3939803dddf84
Walt <walt@gbyte.co>
2018-06-22 21:31:42 +1000

    Convert SingleFragmentActivity to kotlin

--- app/src/main/java/co/gbyte/weightlog/SingleFragmentActivity.kt
+++ app/src/main/java/co/gbyte/weightlog/SingleFragmentActivity.kt
@@ -1,31 +1,23 @@
-package co.gbyte.weightlog;
+package co.gbyte.weightlog
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 
-/**
- * Created by walt on 19/10/16.
- *
- */
+abstract class SingleFragmentActivity : AppCompatActivity() {
 
-public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract fun createFragment(): Fragment
 
-    protected abstract Fragment createFragment();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        val fm = supportFragmentManager
+        var fragment: Fragment? = fm.findFragmentById(R.id.fragment_container)
 
         if (fragment == null) {
-            fragment = createFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+            fragment = createFragment()
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit()
         }
     }
 }

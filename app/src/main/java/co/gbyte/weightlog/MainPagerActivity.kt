9217bfd73b0595b807e889519650ed03d496f0a9
Walt <walt@gbyte.co>
2018-06-22 23:22:40 +1000

    Add Kotlin Android Extensions plugin

--- app/src/main/java/co/gbyte/weightlog/MainPagerActivity.kt
+++ app/src/main/java/co/gbyte/weightlog/MainPagerActivity.kt
@@ -1,10 +1,11 @@
 package co.gbyte.weightlog
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.main_activity_pager.*
+import kotlinx.android.synthetic.main.main_activity_pager.view.*
 
 class MainPagerActivity : AppCompatActivity() {
 
@@ -12,10 +13,11 @@ class MainPagerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_pager)
 
-        val pager = findViewById<ViewPager>(R.id.main_pager)
-        pager!!.adapter = MainPagerAdapter(supportFragmentManager)
+        val pager = main_pager
 
-        val fab = findViewById<FloatingActionButton>(R.id.fab_new_weight)
+        pager.adapter = MainPagerAdapter(supportFragmentManager)
+
+        val fab = pager.fab_new_weight
 
         pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrolled(position: Int,

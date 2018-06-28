0050042bcf1a220a62cdfafa8c66aa79f098dae7
Walt <walt@gbyte.co>
2018-06-28 11:50:39 +1000

    Restore settings and BMI option

--- app/src/main/java/co/gbyte/weightlog/LogFragment.kt
+++ app/src/main/java/co/gbyte/weightlog/LogFragment.kt
@@ -2,7 +2,6 @@ package co.gbyte.weightlog
 
 import android.content.Intent
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -11,20 +10,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import co.gbyte.weightlog.R.id.weight_recycler_view
-
-import java.util.*
 
 import co.gbyte.weightlog.model.Weight
 import co.gbyte.weightlog.model.WeightLab
 
+import java.util.*
+
 import kotlinx.android.synthetic.main.fragment_weight_log.*
 
 class LogFragment : Fragment() {
-    //private var mWeightRecyclerView: RecyclerView? = null
-    //private var mWeight: Weight? = null
-    //private var mMenu: Menu? = null
+    private var mMenu: Menu? = null
+    private var mWeight: Weight? = null
 
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var adapter: RecyclerAdapter
@@ -49,33 +45,15 @@ class LogFragment : Fragment() {
 
         adapter = RecyclerAdapter(weights)
         weight_recycler_view.adapter = adapter
-
-        //mWeightRecyclerView = weight_recycler_view
-        //mWeightRecyclerView?.layoutManager =  LinearLayoutManager(context)
-
-        /*
-        val settings = PreferenceManager.getDefaultSharedPreferences(context)
-        if (!settings.contains(getString(R.string.bmi_pref_key))) {
-            // The app is running for the first time. Ask user for basic settings.
-            showSettings()
-        }
-        updateUI()
-        */
-    }
-
-    override fun onResume() {
-        super.onResume()
-        updateUI()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater!!.inflate(R.menu.fragment_log_menu, menu)
-        //mMenu = menu
+        mMenu = menu
         updateMenu()
     }
 
-    /*
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item!!.itemId) {
             R.id.menu_item_settings -> {
@@ -97,15 +75,14 @@ class LogFragment : Fragment() {
             else -> return super.onOptionsItemSelected(item)
         }
     }
-    */
 
     private fun showSettings() {
         val intent = Intent(context, SettingsActivity::class.java)
         startActivity(intent)
     }
 
+    /*
     private fun updateUI() {
-
         adapter.notifyDataSetChanged()
         // ToDo: find out if we still should:
         // switch to:
@@ -113,9 +90,10 @@ class LogFragment : Fragment() {
         // The challenge may be discovering which position has
         // changed and reloading the correct item.
     }
+    */
 
     private fun updateMenu() {
-        //val editMenuItem = mMenu?.findItem(R.id.menu_item_edit_weight)
-        //editMenuItem?.isVisible = mWeight != null
+        val editMenuItem = mMenu?.findItem(R.id.menu_item_edit_weight)
+        editMenuItem?.isVisible = mWeight != null
     }
 }

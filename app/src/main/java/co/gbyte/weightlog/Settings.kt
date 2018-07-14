b69e6b0a156823e4f1ef5bce0a3e20c2057fe2b9
Walt <walt@gbyte.co>
2018-07-15 07:08:10 +1000

    Move settings implementation to object Settings

--- app/src/main/java/co/gbyte/weightlog/Settings.kt
+++ app/src/main/java/co/gbyte/weightlog/Settings.kt
@@ -5,11 +5,9 @@ import android.content.SharedPreferences
 
 object Settings {
 
-    private const val NAME = "SpinKotlin"
+    private const val NAME = "WeightLog"
     private const val MODE = Context.MODE_PRIVATE
-    private lateinit var preferences: SharedPreferences
-
-    private val IS_FIRST_RUN_PREF = Pair("is_first_run", false)
+    lateinit var preferences: SharedPreferences
 
     fun init(context: Context) {
         preferences = context.getSharedPreferences(NAME, MODE)
@@ -26,12 +24,22 @@ object Settings {
         editor.apply()
     }
 
-    var firstRun: Boolean
+    private const val SHOW_BMI_KEY = "show_bmi_key"
+    private val SHOW_BMI = Pair(SHOW_BMI_KEY, false)
+    var showBmi: Boolean
         // custom getter to get a preference of a desired type, with a predefined default value
-        get() = preferences.getBoolean(IS_FIRST_RUN_PREF.first, IS_FIRST_RUN_PREF.second)
-
+        get() = preferences.getBoolean(SHOW_BMI.first, SHOW_BMI.second)
         // custom setter to save a preference back to preferences file
         set(value) = preferences.edit {
-            it.putBoolean(IS_FIRST_RUN_PREF.first, value)
+            it.putBoolean(SHOW_BMI.first, value)
+        }
+
+    private const val HEIGHT_KEY = "height"
+    private const val INITIAL_HEIGHT = 160
+    private val HEIGHT = Pair(HEIGHT_KEY, INITIAL_HEIGHT)
+    var height: Int
+        get() = preferences.getInt(HEIGHT.first, HEIGHT.second)
+        set(value) = preferences.edit {
+            it.putInt(SHOW_BMI.first, value)
         }
 }

55790783d9b8c419c646efc96dfe4bc79ffe65e4
Walt <walt@gbyte.co>
2018-07-17 12:14:40 +1000

    Implement utils.Time.* as ext. to java.util.Date

--- app/src/main/java/co/gbyte/weightlog/Extensions.kt
+++ app/src/main/java/co/gbyte/weightlog/Extensions.kt
@@ -1,5 +1,6 @@
 package co.gbyte.weightlog
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+
+import android.text.format.DateFormat
+import java.text.SimpleDateFormat
+
+import java.util.*
+
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false) : View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
@@ -23,5 +30,19 @@ fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int) {
 
 fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
     supportFragmentManager.inTransaction { replace(frameId, fragment) }
+}
+
+/**
+ * Former Utils.Time
+ */
+fun Date.getTimeString(context: Context) : String
+        = DateFormat.getTimeFormat(context).format(this)
+
+fun Date.getShortDateString(context: Context) :String
+        = DateFormat.getDateFormat(context).format(this)
+
+fun Date.getDateString(context: Context, format: String) : String {
+    return (SimpleDateFormat(format, Locale.getDefault()).format(this)
+            + DateFormat.getMediumDateFormat(context).format(this))
 }
 

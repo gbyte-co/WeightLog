5fcfe55a1846b9f9ee3a6b4e5a99d2dd504cb8ff
Walt <walt@gbyte.co>
2018-07-18 22:14:09 +1000

    Replace member vars with non-nullable local ones

--- app/src/main/java/co/gbyte/weightlog/WeightPickerFragment.kt
+++ app/src/main/java/co/gbyte/weightlog/WeightPickerFragment.kt
@@ -12,35 +12,33 @@ import androidx.fragment.app.DialogFragment
 import co.gbyte.android.weightpicker.WeightPicker
 
 class WeightPickerFragment : DialogFragment() {
-    private var mWeight: Int = 0
-    private var mWeightPicker: WeightPicker? = null
 
     @NonNull
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        mWeight = arguments!!.getInt(ARG_WEIGHT)
 
-        val v = LayoutInflater.from(context)
-                .inflate(R.layout.dialog_weight, null)
+        var weight = arguments?.getInt(ARG_WEIGHT)  ?: 0
 
-        mWeightPicker = v.findViewById(R.id.dialog_weight_weight_picker)
-        mWeightPicker!!.value = mWeight
-        mWeightPicker!!.onValueChangedListener =
+        val v = LayoutInflater.from(context).inflate(R.layout.dialog_weight, null)
+        val weightPicker = v.findViewById<WeightPicker>(R.id.dialog_weight_weight_picker)
+
+        weightPicker.value = weight
+        weightPicker!!.onValueChangedListener =
                 WeightPicker.OnValueChangedListener {
-            picker, oldValue, newValue -> mWeight = mWeightPicker!!.value
-            arguments!!.putInt(EXTRA_WEIGHT, mWeight)
+            picker, oldValue, newValue -> weight = weightPicker.value
+            arguments?.putInt(EXTRA_WEIGHT, weight)
         }
 
-        mWeightPicker!!.clearFocus()
+        weightPicker.clearFocus()
         return AlertDialog.Builder(activity)
                 .setView(v)
                 .setTitle(R.string.weight_picker_title)
                 .setNegativeButton(android.R.string.cancel)
-                        { dialog, whichButton -> sendResult(Activity.RESULT_CANCELED, mWeight) }
+                        { dialog, whichButton -> sendResult(Activity.RESULT_CANCELED, weight) }
                 .setPositiveButton(android.R.string.ok
                 ) { dialogInterface, i ->
                     // needed when user edits the text field and clicks OK:
-                    mWeightPicker!!.clearFocus()
-                    sendResult(Activity.RESULT_OK, mWeight)
+                    weightPicker.clearFocus()
+                    sendResult(Activity.RESULT_OK, weight)
                 }
                 .create()
     }

c0cd0f6962f6b6ac2a1d373d1b89006693849a11
Walt <walt@gbyte.co>
2018-07-23 00:14:49 +1000

    Convert db/WeightBaseHelper to kotlin

--- app/src/main/java/co/gbyte/weightlog/db/WeightBaseHelper.kt
+++ app/src/main/java/co/gbyte/weightlog/db/WeightBaseHelper.kt
@@ -1,33 +1,30 @@
-package co.gbyte.weightlog.db;
+package co.gbyte.weightlog.db
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 
-import co.gbyte.weightlog.db.WeightDbSchema.WeightTable;
+import co.gbyte.weightlog.db.WeightDbSchema.WeightTable
 
-public class WeightBaseHelper extends SQLiteOpenHelper{
-    private static final int VERSION = 1;
-    private static final String DB_NAME = "weightBase.db";
+class WeightBaseHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, VERSION) {
 
-    public WeightBaseHelper(Context context) {
-        super(context, DB_NAME, null, VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
+    override fun onCreate(db: SQLiteDatabase) {
         db.execSQL("create table " + WeightTable.NAME + "(" +
-                "_id integer primary key autoincrement, "   +
-                WeightTable.Cols.UUID   + " TEXT, " +
-                WeightTable.Cols.TIME   + " TEXT, " +
-                WeightTable.Cols.WEIGHT + " INT, "  +
-                WeightTable.Cols.NOTE   + " TEXT"   +
+                "_id integer primary key autoincrement, " +
+                WeightTable.Cols.UUID + " TEXT, " +
+                WeightTable.Cols.TIME + " TEXT, " +
+                WeightTable.Cols.WEIGHT + " INT, " +
+                WeightTable.Cols.NOTE + " TEXT" +
                 ")"
-        );
+        )
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
 
+    }
+
+    companion object {
+        private const val VERSION = 1
+        private const val DB_NAME = "weightBase.db"
     }
 }

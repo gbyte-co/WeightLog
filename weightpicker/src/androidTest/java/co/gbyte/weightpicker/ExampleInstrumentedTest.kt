36c9274c509e529a592b4b4443e5434e5a406dbc
Walt <walt@gbyte.co>
2018-12-12 22:40:50 +1100

    Convert sample tests to Kotlin

---
+++ weightpicker/src/androidTest/java/co/gbyte/weightpicker/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package co.gbyte.weightpicker
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("co.gbyte.weightpicker2.test", appContext.getPackageName())
+    }
+}
